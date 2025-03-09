459ca401e35ea41ce95d1a30fc4318c9cf65469a
isaurus <isaacmartin.dev@gmail.com>
2025-03-09 16:42:59 +0100

    First commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation (com.google.android.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
